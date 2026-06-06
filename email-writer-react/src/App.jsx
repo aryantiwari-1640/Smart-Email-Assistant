f9f0df124b07bcd5a125d73be0ef643067c8673f
aryantiwari-1640 <aryantiwari.1640@gmail.com>
2026-06-06 19:33:18 +0530

    Adding vercel file

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     setError('');
 
     try {
-      const response = await axios.post('/api/email/generate', {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/email/generate`, {
         emailContent,
         tone
       });
